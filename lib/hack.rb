a2872e6d897bfd4c22c67995fef3a10b7c9c876d
Michael Chui <saraid216@gmail.com>
2021-11-10 13:35:57 -0800

    convert script to ruby

---
+++ lib/hack.rb
@@ -0,0 +1,22 @@
+require 'kramdown'
+
+Dir.chdir(`git rev-parse --show-toplevel`.chomp)
+pages = `find pages -name \*.page`.split($/)
+
+kramdown_options = {
+  template: 'templates/default.template'
+}
+
+pages.each do |input_filename|
+  source_text = File.read(input_filename)
+  output_filename = input_filename.yield_self do |filename|
+    parts = filename.split('/')
+    parts[0] = 'docs'
+    parts[-1] = parts[-1].sub(/\.page$/, '.html')
+    parts.join('/')
+  end
+
+  document = Kramdown::Document.new(source_text, kramdown_options)
+  File.open(output_filename, 'w') { |f| f.write(document.to_html) }
+end
+
